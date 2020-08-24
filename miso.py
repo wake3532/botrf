2702dfb5dc86f2702cc923f7ab6aeb7c5576d429
wake3532 <70120646+wake3532@users.noreply.github.com>
2020-08-24 12:21:52 +0900

    Update miso.py

--- miso.py
+++ miso.py
@@ -2,11 +2,9 @@ import discord
 from discord.ext import commands
 import os
 import asyncio
-import bs4 
 import random
 import urllib
 from urllib.request import Request
-from bs4 import BeautifulSoup
 from urllib import parse
 
 
